f0310e6748452ed8340d41b2db179262ae41832f
alireza aa <aalaee1383@gmail.com>
2023-04-20 16:42:55 +0430

    first commit

---
+++ StrongHold/src/Moudel/User.java
@@ -0,0 +1,11 @@
+package Moudel;
+
+public class User {
+    private String userName;
+    private String password;
+    private String nickName;
+    private String email;
+    private String slogan;
+
+
+}
